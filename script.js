416eee78ff2ee9bbe5e06665ff9afa625e73ab0f
mehmetcandinc <dincmehmetcann@gmail.com>
2024-04-28 20:09:15 +0300

    UIKit

---
+++ script.js
@@ -0,0 +1,13 @@
+/*NOTIFICATION*/
+window.onload = function () {
+  UIkit.notification({
+    message:
+      "<span class='uk-text-warning uk-text-meta'> 7.Notification</span>",
+    timeout: 2400,
+  });
+  UIkit.notification({
+    message:
+      "Bu sitenin <span class='uk-text-primary'> tamamı UIKit </span> materyalleri ile hazırlanmıştır.",
+    timeout: 3000,
+  });
+};
